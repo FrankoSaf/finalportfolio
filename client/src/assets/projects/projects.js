3964e884bc209daafb32a0284b43080d6bcb96ed
FrankoSaf <franko.safradin@gmail.com>
2022-12-14 16:45:34 +0100

    added another project

--- client/src/assets/projects/projects.js
+++ client/src/assets/projects/projects.js
@@ -20,6 +20,24 @@ export const projects = [
     ],
   },
   {
+    name: 'Foodridge',
+    links: [
+      'https://github.com/FrankoSaf/foodridge',
+      'https://heroic-torrone-0d3037.netlify.app/',
+    ],
+    description:
+      'Simple app that enables the user to find recipes based on ingredients. User enters ingredients that he has in fridge. If app finds a recipe that has exact ingredient, it will show it to the user. If not, it will show the user recipe and up to 3 ingredients that user need to buy',
+    process:
+      'App is built with Vite.js React template, it is based on Spoonacular API. App has 150 calls to API, in case it is not working, feel free to contact me, and I will give you another API. This is the first version.',
+    technologies: [
+      'React',
+      'HTML',
+      'CSS',
+      'Styled Components',
+      'Spoonacular API',
+    ],
+  },
+  {
     name: 'Retro Portfolio',
     links: [
       'https://github.com/FrankoSaf/retroportfolio',
